4c865fe9ef5550786691a3700c69f5277c8042a6
kalyangoud145 <kalyangoud145@gmail.com>
2020-11-04 14:02:58 +0530

    [Kalyan] added tc for maximum number method

--- MSTest.cs
+++ MSTest.cs
@@ -6,17 +6,31 @@ namespace FindMaxNumberMSTest
     [TestClass]
     public class MSTest
     {
-        ///Tc 1.1
+        ///TC 1.1
         /// <summary>
-        /// Givens the integer  to find maximum integer return Expected value
+        /// Givens the integer to find maximum integer given maximum int value at first position return expected value.
         /// </summary>
         [TestMethod]
-        public void Given_Integer_ToFindMaximumInteger_ReturnExpectedValue()
+        public void Given_Integer_ToFindMaximumInteger_Given_MaximumIntValueAtFirstPosition_Should_ReturnExpectedValue()
         {
             //Arrange
             int expected = 10;
             //Act
             int actual = MaximumNumberCheck.MaximumIntegerNumber(10, 9, 8);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        ///TC 1.2
+        /// <summary>
+        /// Givens the integer to find maximum integer ,maximum number at second position return expected value.
+        /// </summary>
+        [TestMethod]
+        public void Given_Integer_ToFindMaximumInteger_MaximumNumber_AtSecondPosition_ReturnExpectedValue()
+        {
+            //Arrange
+            int expected = 10;
+            //Act
+            int actual = MaximumNumberCheck.MaximumIntegerNumber(7, 10, 8);
             //Assert
             Assert.AreEqual(expected, actual);
         }
