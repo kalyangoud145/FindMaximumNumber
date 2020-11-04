332c2a9d658bfe79e07dcf441ecc9e4bcd9536e3
kalyangoud145 <kalyangoud145@gmail.com>
2020-11-04 14:28:42 +0530

    [Kalyan] added method to find maximum of three float numbers

--- MaximumNumberCheck.cs
+++ MaximumNumberCheck.cs
@@ -33,5 +33,31 @@ namespace FindMaximumNumber
             }
             throw new Exception("All three values are same");
         }
+        /// <summary>
+        /// Returns Maximum the float number of given values
+        /// </summary>
+        /// <param name="firstValue">The first value.</param>
+        /// <param name="secondValue">The second value.</param>
+        /// <param name="thirdValue">The third value.</param>
+        /// <returns></returns>
+        /// <exception cref="System.Exception">All three values are same</exception>
+        public static float MaximumFloatNumber(float firstValue, float secondValue, float thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                return thirdValue;
+            }
+
+            throw new Exception("All three values are same");
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -11,6 +11,8 @@ namespace FindMaximumNumber
         {
             int intOutput = MaximumNumberCheck.MaximumIntegerNumber(80, 40, 20);
             Console.WriteLine("The maximum integer number is: " + intOutput);
+            float floatOutput = MaximumNumberCheck.MaximumFloatNumber(101.523f, 150.325f, 100.625f);
+            Console.WriteLine("The maximum float number is: " + floatOutput);
         }
     }
 }
